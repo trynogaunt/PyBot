64da751dc47f587d09f948a07f8f4709d97b353f
Tryno <toussaint.quentin00@gmail.com>
2026-02-18 11:31:50 +0100

    Refactor connect method to include schema creation in DatabasePool

--- bot/db/pool.py
+++ bot/db/pool.py
@@ -13,7 +13,24 @@ class DatabasePool:
         if not self.database_url:
             raise ValueError("DATABASE_URL is not set.")
         self.pool = await asyncpg.create_pool(self.database_url)
+        await self.scheme_check()
 
     async def disconnect(self):
         if self.pool:
             await self.pool.close()
+
+    async def scheme_check(self):
+        if not self.pool:
+            raise ValueError("Database pool is not initialized.")
+        async with self.pool.acquire() as connection:
+            await connection.execute(
+                """
+                CREATE SCHEMA IF NOT EXISTS core;
+            """
+            )
+
+            await connection.execute(
+                """
+                CREATE SCHEMA IF NOT EXISTS features;
+            """
+            )
